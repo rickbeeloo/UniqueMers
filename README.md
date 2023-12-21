5ebb996c34aaed4b92df52eec504d4f71ff5c5fe
rickbeeloo <r.beeloo@outlook.com>
2023-12-21 14:44:21 +0100

    Speed up + readme

---
+++ README.md
@@ -0,0 +1,48 @@
+
+# Sequences containing unique kmers
+
+Not that interesting, but we recursively mutate a kmer to generate unique kmer sequences till a specfified maximum number of sequences is reached. It generates `max_kmers` unique kmers of size `k` and save them to a fasta, `fasta_output`. Alternativly a last argument can be passed `tail_length` to append a random sequence of `<= tail_length` to the end of the kmer.
+
+
+## Usage
+
+`UniqueMers <fasta_output> <kmer_length> <max_kmers> <tail_length>`
+
+
+## Installation
+
+```
+git clone https://github.com/rickbeeloo/UniqueMers
+cd UniqueMers/
+cargo build --release
+```
+
+The executable should now be in `/target/release/`. For example on Windows this would be `./target/release/UniqueMers.exe` on windows.
+
+## Example 
+if we run `UniqueMers.exe kmers.fasta 3 5 5` the `kmers.fasta` looks like:
+
+```
+>seq1
+AAA
+>seq2
+AACGTGAT
+>seq3
+AAGCTA
+>seq4
+AATG
+>seq5
+ACAGT
+```
+
+Note that while for example `seq1` is exactly the kmer size (`3`) some sequences are longer cause of the tail being `5`. For example `seq2` is `k + 5`.
+
+## Performance
+If we use `k=17` and a tail size of `10`:
+```
+1 thousand sequences: 0m0.047s
+1 million sequences: 0m0.393s
+10 million sequences: 0m1.215s
+1 billion sequences: 1m56.440s
+```
+(real times reported by `time`)
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,97 @@
 use std::env;
 use std::fs::File;
-use std::io::{self, Write};
+use std::io::{self, Write, BufWriter};
+use rand::Rng;
+use rand::seq::SliceRandom;
 
 fn generate_kmers_recursive(
     current_kmer: &str,
     length: usize,
-    nucleotides: &[char],
-    file: &mut File,
+    nucleotides: &[u8], 
+    file: &mut BufWriter<File>,
     max_kmers: usize,
+    tail_length: usize,
+    rng: &mut impl Rng,
     i: &mut usize,
+    reusable_tail: &mut Vec<u8>, 
+    buffer: &mut Vec<u8>, 
 ) {
-    if *i >= max_kmers {
+    // Done if we reached max kmers
+    if *i > max_kmers {
         return;
     }
 
     if length == 0 {
-        writeln!(file, ">kmer{}", *i).expect("Write failed");
-        writeln!(file, "{}", current_kmer).expect("Write failed");
+        generate_random_tail(tail_length, nucleotides, rng, reusable_tail);
+        buffer.clear();
+        buffer.extend_from_slice(current_kmer.as_bytes());
+        buffer.extend_from_slice(&reusable_tail);
+        writeln!(file, ">seq{}", *i).expect("Write failed");
+        file.write_all(&buffer).expect("Write failed");
         *i += 1;
     } else {
         for &nucleotide in nucleotides {
-            let new_kmer = format!("{}{}", current_kmer, nucleotide);
-            generate_kmers_recursive(&new_kmer, length - 1, nucleotides, file, max_kmers, i);
+            reusable_tail.push(nucleotide);
+            generate_kmers_recursive(
+                current_kmer,
+                length - 1,
+                nucleotides,
+                file,
+                max_kmers,
+                tail_length,
+                rng,
+                i,
+                reusable_tail,
+                buffer,
+            );
+            reusable_tail.pop();
         }
+    }
+}
+
+// Generate a random tail for the chosen kmer size
+fn generate_random_tail(max_tail_size: usize, nucleotides: &[u8], rng: &mut impl Rng, tail: &mut Vec<u8>) {
+    tail.clear(); // Clear the tail before reuse
+    let tail_size = rng.gen_range(0..=max_tail_size);
+
+    for _ in 0..tail_size {
+        tail.push(*nucleotides.choose(rng).unwrap());
     }
 }
 
 fn generate_kmers(
     kmer_length: usize,
     max_kmers: usize,
-    file: &mut File,
-) -> Vec<String> {
-    let nucleotides = ['A', 'C', 'G', 'T'];
-    let mut kmers = Vec::new();
+    tail_length: usize,
+    file: &mut BufWriter<File>,
+) {
+    let nucleotides: Vec<u8> = b"ACGT".to_vec(); // Use u8 instead of char
     let mut i = 1;
+    let mut rng = rand::thread_rng(); // Initialize the random number generator
+    let mut reusable_tail = Vec::with_capacity(tail_length);
+    let mut buffer = Vec::new(); // Reusable buffer for writing
 
-    generate_kmers_recursive("", kmer_length, &nucleotides, file, max_kmers, &mut i);
-
-    kmers
+    generate_kmers_recursive(
+        "",
+        kmer_length,
+        &nucleotides,
+        file,
+        max_kmers,
+        tail_length,
+        &mut rng,
+        &mut i,
+        &mut reusable_tail,
+        &mut buffer,
+    );
 }
 
-fn write_fasta(filename: &str, kmers: &[String]) -> io::Result<()> {
-    let mut file = File::create(filename)?;
-
-    for (i, kmer) in kmers.iter().enumerate() {
-        writeln!(file, ">kmer{}", i + 1)?;
-        writeln!(file, "{}", kmer)?;
-    }
-
-    Ok(())
+// Check if we can generate sufficient kmers with the given k size
+fn can_generate_kmers(k: usize, max_kmers: usize) -> bool {
+    let num_nucleotides: u64 = 4; 
+    println!("k: {}, and max: {}", k, max_kmers);
+    let total_kmers: usize = num_nucleotides.pow(k as u32) as usize;
+    println!("total mers: {}", total_kmers);
+    max_kmers <= total_kmers
 }
 
 fn main() -> io::Result<()> {
@@ -56,21 +99,27 @@ fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     // Check if the number of arguments is correct
-    if args.len() != 3 {
-        println!("Usage: {} <kmer_length> <max_kmers>", args[0]);
+    if args.len() != 5 {
+        println!("Usage: {} <fasta_output> <k> <max_kmers> <tail_length>", args[0]);
         return Ok(());
     }
 
-    // Output
-    let fasta_filename = "unique_kmers.fasta";
-    let mut file = File::create(&fasta_filename).expect("Could not open output file");
-
     // Parse command-line arguments
-    let kmer_length: usize = args[1].parse().expect("Invalid kmer_length");
+    let fasta_filename = &args[1];
+    let kmer_length: usize = args[2].parse().expect("Invalid kmer_length");
+    let max_kmers: usize = args[3].parse().expect("Not an int");
+    let tail_length: usize = args[4].parse().expect("Invalid tail_length");
 
-    let max_kmers: usize = args[2].parse().expect("Not an int");
+    // Does this make sense
+    if !can_generate_kmers(kmer_length, max_kmers) {
+        panic!("Cannot generate sufficient unique kmers, increase k");
+    }
 
-    generate_kmers(kmer_length, max_kmers, &mut file);
+    // Replaced with BufferedWriter to make writing a bit faster
+    let file = File::create(&fasta_filename).expect("Could not open output file");
+    let mut buffered_file = BufWriter::new(file);
+
+    generate_kmers(kmer_length, max_kmers, tail_length, &mut buffered_file);
 
     // Print the total number of unique k-mers
     println!("FASTA file saved as: {}", fasta_filename);
